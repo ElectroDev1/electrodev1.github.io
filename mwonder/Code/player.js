b4266222598f0c67bdd404f6173ec2ecc336fc26
Electrodev <electrogamesbusinnes@gmail.com>
2023-11-05 22:20:44 +0100

    art additions

--- mwonder/Code/player.js
+++ mwonder/Code/player.js
@@ -1,1 +1,36 @@
-import yaml from "js-yaml"
+const mainPath = './Data/Player/'
+class Player {
+    constructor(name){
+        this.name = name
+        this.path = mainPath+name
+    }
+}
+
+
+const players = [
+
+    new Player("Mario")
+
+]
+
+
+const dataNames = ["carry","collision","eat","elephant","item","jump","slope","speed","swim"]
+/*
+for(let i = 0; i < players.length; i++){
+
+
+
+    var player = players[i]
+
+    console.log("Loading player "+player.name)
+
+    for(let j = 0; j < dataNames.length; j++){
+
+        loadJSONtoObj(player, dataNames[j], player.path+"/"+dataNames[j]+".json" )
+
+      
+
+    }
+}*/
+
+loadJSONtoObj(players[0],"speed", players[0].path+"/speed.json")
